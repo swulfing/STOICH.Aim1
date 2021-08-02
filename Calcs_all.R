3a7ab48eb968c37eac0a21ab2d316c82b4ac02d9
Sophie <sophiewulfing@gmail.com>
2021-08-02 11:45:21 -0700

    Presentation Edits

--- Calcs_all.R
+++ Calcs_all.R
@@ -1,4 +1,9 @@
+library("dplyr")
 library("ggplot2")
+library("rnaturalearth")
+library("rnaturalearthdata")
+library("VennDiagram")
+library("lme4")
 
 brazil <-data.frame(read.csv("C:/Users/sophi/Documents/STOICH/STOICH.Aim1/MedianCalcs/BrazilData_Medians.csv"))
 Chla <-data.frame(read.csv("C:/Users/sophi/Documents/STOICH/STOICH.Aim1/MedianCalcs/ChlaData_Medians.csv"))
@@ -10,28 +15,13 @@ NLArs2019 <-data.frame(read.csv("C:/Users/sophi/Documents/STOICH/STOICH.Aim1/Med
 NLAs2004 <-data.frame(read.csv("C:/Users/sophi/Documents/STOICH/STOICH.Aim1/MedianCalcs/NLAs2004_Medians.csv"))
 
 ##All Data##
-
-#DOC TP
-All.DOC.TP<- ggplot(NULL, aes(DOC_Median, TP_Median))+
-  geom_point(data = NLA2012, col = "blue", pch = 1)+
-  geom_point(data = NLA2017, col = "blue", pch = 1)+
-  geom_point(data = NLArs2008, col = "green", pch = 1)+
-  geom_point(data = NLArs2013, col = "green", pch = 1)+
-  geom_point(data = NLArs2019, col = "green", pch = 1)+
-  geom_point(data = NLAs2004, col = "green", pch = 1)+
-  labs(x = "DOC",
-       y = "TP")
-
-All.DOC.TP  
-
-
 #DOC TN
 Rivers.Nitrate.DOC.NO3<- ggplot(NULL, aes(DOC_Median, NO3_Median))+
   geom_point(data = NLArs2008, col = "green", pch = 1)+
   geom_point(data = NLArs2013, col = "green", pch = 1)+
   geom_point(data = NLArs2019, col = "green", pch = 1)+
   geom_point(data = NLAs2004, col = "green", pch = 1)+
-  xlim(0,50)+
+  xlim(0,20)+
   ylim(0,10000)+
   labs(x = "DOC",
        y = "NO3")
@@ -47,20 +37,6 @@ Lakes.Nitrate.DOC.NO3<- ggplot(NULL, aes(DOC_Median, NO3_Median))+
        y = "NO3")
 
 Lakes.Nitrate.DOC.NO3
-
-#TN to TP
-Nitrate.TP<- ggplot(NULL, aes(NO3_Median, TP_Median))+
-  geom_point(data = NLArs2008, col = "green", pch = 1)+
-  geom_point(data = NLArs2013, col = "green", pch = 1)+
-  geom_point(data = NLArs2019, col = "green", pch = 1)+
-  geom_point(data = NLAs2004, col = "green", pch = 1)+
-  geom_point(data = NLA2012, col = "blue", pch = 1)+
-  geom_point(data = NLA2017, col = "blue", pch = 1)+
-  labs(x = "NO3",
-       y = "TP")
-
-Nitrate.TP
-
 
 #Doc to NP Ratio
 #DOC TN
@@ -85,3 +61,56 @@ Lakes.Nitrate.DOC.TNTP<- ggplot(NULL, aes(DOC_Median, NO3toTP_Median))+
        x = "DOC")
 
 Lakes.Nitrate.DOC.TNTP
+
+#DOC TP
+All.DOC.TP<- ggplot(NULL, aes(DOC_Median, TP_Median))+
+  geom_point(data = NLA2012, col = "blue", pch = 1)+
+  geom_point(data = NLA2017, col = "blue", pch = 1)+
+  geom_point(data = NLArs2008, col = "green", pch = 1)+
+  geom_point(data = NLArs2013, col = "green", pch = 1)+
+  geom_point(data = NLArs2019, col = "green", pch = 1)+
+  geom_point(data = NLAs2004, col = "green", pch = 1)+
+  labs(x = "DOC",
+       y = "TP")
+All.DOC.TP
+
+#TN to TP
+Nitrate.TP<- ggplot(NULL, aes(TP_Median, NO3_Median))+
+  #geom_point(data = NLArs2008, col = "green", pch = 1)+
+  #geom_point(data = NLArs2013, col = "green", pch = 1)+
+  #geom_point(data = NLArs2019, col = "green", pch = 1)+
+  #geom_point(data = NLAs2004, col = "green", pch = 1)+
+  geom_point(data = NLA2012, col = "blue", pch = 1)+
+  geom_point(data = NLA2017, col = "blue", pch = 1)+
+  xlim(0,2250)+
+  ylim(0,7)+
+  labs(x = "TP",
+       y = "NO3")
+
+Nitrate.TP
+
+
+##Mapping##
+
+#datapoints <- read.csv("C:/Users/sophi/Documents/STOICH/STOICH.Aim1/MedianCalcs/NLArs2013_Medians.csv")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+class(world)
+
+
+ggplot(data = world) +
+  geom_sf() +
+#  geom_point(data = NLA2017, aes(x = Lon, y = Lat), size = 1, 
+#             shape = 23, fill = "blue")+
+#  geom_point(data = NLArs2008, aes(x = Lon, y = Lat), size = 1, 
+#             shape = 23, fill = "blue")+
+  geom_point(data = NLArs2013, aes(x = Lon, y = Lat), size = .5, 
+             shape = 23, fill = "green")+
+  geom_point(data = NLA2012, aes(x = Lon, y = Lat), size = .5, 
+             shape = 23, fill = "blue")+
+#  geom_point(data = NLArs2019, aes(x = Lon, y = Lat), size = 1, 
+#             shape = 23, fill = "blue")+
+  geom_point(data =brazil, aes(x = Lon, y = Lat), size = .5, 
+             shape = 23, fill = "green")
+#  geom_point(data =NLAs2004, aes(x = Lon, y = Lat), size = 1, 
+#             shape = 23, fill = "blue")
