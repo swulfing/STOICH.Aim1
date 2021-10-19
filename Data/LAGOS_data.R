607a10b443b6beeffb815bf746b23f6a4e9e22b5
LinneaRock <lrock@wisc.edu>
2021-10-19 13:40:09 -0600

    LAR: added folder for datasets, put LAGOS script in folder. There are 3 questions in there for us to talk about later!

---
+++ Data/LAGOS_data.R
@@ -0,0 +1,27 @@
+#####Load packages####
+
+library(tidyverse)
+library(LAGOSNE)
+
+####The database#####
+
+#https://lagoslakes.org/
+
+#Download the LAGOS dataset - you only need to do this once on your computer. This can take a couple minutes.
+##lagosne_get(dest_folder = lagos_path())
+
+#Then load in the dataset. Do this everytime you want to use LAGOS
+lagos <- lagosne_load()
+
+#This next line will bring up the help pages for the package
+#if you look in epi_nutri, you can see all the water quality parameters available
+help.search("datasets", package = "LAGOSNE")
+
+####Filter for DOC, Nitrate, TP data####
+#Question -- do we want total dissolved P? or just TP? Maybe depends on what is avaialable from other data sources?
+#Question -- can we use nitrite, nitrate - N? 
+#Question -- do we only want to keep rows that have all 3: DOC, nitrite/nitrate, TP?
+
+lagos_data <- lagos$epi_nutr %>%
+  select(lagoslakeid, sampledate, doc, no2no3, tp, tdp)
+
