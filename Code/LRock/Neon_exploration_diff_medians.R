31b2fb020f10c1838e492959b730dbf3662274bc
LinneaRock <lrock@wisc.edu>
2021-11-08 15:15:53 -0700

    looking at NEON data with medians for 1)all time per site, 2)daily median per site, 3)annual median per site

---
+++ Code/LRock/Neon_exploration_diff_medians.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(lubridate)
+
+neon <- read.csv("Data/NEON/SIMPLE_SURFACE_WATER_CHEMS_DATA.csv")
+
+
+#total median of all time per site####
+median_neon <- neon %>%
+  group_by(siteID, analyte) %>%
+  mutate(median_concentration = median(analyteConcentration)) %>%
+  ungroup() %>%
+  select(siteID, analyte, median_concentration, analyteUnits) %>%
+  distinct() %>%
+  pivot_wider(names_from = analyte, values_from = median_concentration) %>%
+  rename(no2no3 = "NO3+NO2 - N")
+
+
+ggplot(median_neon) +
+  geom_point(aes(DOC, no2no3, color = TP)) +
+ # scale_y_log10() +
+  scale_color_viridis_c(direction = -1) +
+  theme_bw() +
+  labs(x = "DOC"~mg~L^-1,
+       y = "Nitrate"~mg~L^-1)
+
+
+
+
+#median per date, site####
+daily_median_neon <- neon %>%
+  group_by(siteID, DateTime, analyte) %>%
+  mutate(median_concentration = median(analyteConcentration)) %>%
+  ungroup() %>%
+  select(-analyteConcentration, -X, -sampleCondition) %>%
+  distinct() %>%
+  pivot_wider(id_cols = c(siteID, DateTime), names_from = analyte, values_from = median_concentration) %>%
+  rename(no2no3 = "NO3+NO2 - N") 
+
+
+ggplot(daily_median_neon) +
+  geom_point(aes(DOC, no2no3, color = TP)) +
+  #scale_y_log10() +
+  scale_color_viridis_c(direction = -1) +
+  theme_bw() +
+  labs(x = "DOC"~mg~L^-1,
+       y = "Nitrate"~mg~L^-1)
+
+
+
+
+#annual median per site####
+annual_median_neon <- neon %>%
+  mutate(year = year(DateTime)) %>%
+  group_by(siteID, year, analyte) %>%
+  mutate(median_concentration = median(analyteConcentration)) %>%
+  ungroup() %>%
+  select(-analyteConcentration, -X, -sampleCondition) %>%
+  distinct() %>%
+  pivot_wider(id_cols = c(siteID, DateTime), names_from = analyte, values_from = median_concentration) %>%
+  rename(no2no3 = "NO3+NO2 - N") 
+
+
+ggplot(annual_median_neon) +
+  geom_point(aes(DOC, no2no3, color = TP)) +
+  #scale_y_log10() +
+  scale_color_viridis_c(direction = -1) +
+  theme_bw() +
+  labs(x = "DOC"~mg~L^-1,
+       y = "Nitrate"~mg~L^-1)
