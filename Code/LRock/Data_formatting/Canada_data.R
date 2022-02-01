55baa2082091938309e05347cb8c60d65ae3a470
LinneaRock <lrock@wisc.edu>
2022-01-31 17:52:53 -0700

    updated Canada data. Now dataset in Data/Simplified_datasets.. contains PO4 rather than TP. We went from 127 sites to 10. Also added dataset with all other variables measured along with DOC, NO3, and PO4 at those sites. Added metadata files to the NRCanada raw data file

--- Code/LRock/Data_formatting/Canada_data.R
+++ Code/LRock/Data_formatting/Canada_data.R
@@ -4,26 +4,25 @@ library(tidyverse)
 library(data.table)
 
 getwd()
-#temporarily set working directory to file path
-setwd("C:/Users/lrock1/OneDrive - University of Wyoming/PhD_code/STOICH.Aim1/Data/Natural_Resources_Canada_Data")
+
 
 #create a list of the files from your target directory
-file_list <- list.files(path="C:/Users/lrock1/OneDrive - University of Wyoming/PhD_code/STOICH.Aim1/Data/Natural_Resources_Canada_Data")
+file_list <- list.files(path="Data/Natural_Resources_Canada_Data/raw_data")
+file_list <- paste("C:/Users/linne/OneDrive - University of Wyoming/PhD_code/STOICH_Aim1/Data/Natural_Resources_Canada_Data/raw_data/", file_list, sep = "")
 
 #initiate a blank data frame, each iteration of the loop will append the data from the given file to this variable
 chems_dataset <- data.frame()
 
 #had to specify columns to get rid of the total column
 for (i in 1:length(file_list)){
-  temp_chems_data <- fread(file_list[i], stringsAsFactors = F) %>% #read in files using the fread function from the data.table package
+  temp_chems_data <- fread(file_list[i], stringsAsFactors = F) #read in files using the fread function from the data.table package
   chems_dataset <- rbindlist(list(chems_dataset, temp_chems_data), use.names = T, fill = TRUE) #for each iteration, bind the new data to the building dataset
 }
 
 params <- as.data.frame(unique(chems_dataset$VARIABLE))
 units <- as.data.frame(unique(chems_dataset$UNIT_UNITÉ))
 
-#set working directory back to the main project file
-setwd("C:/Users/lrock1/OneDrive - University of Wyoming/PhD_code/STOICH.Aim1")
+
 
 #filter to keep only the necessary data and columns
 CA_data <- chems_dataset %>%
@@ -33,21 +32,29 @@ CA_data <- chems_dataset %>%
            VARIABLE == "NITROGEN, NITRATE" |
            VARIABLE == "NITROGEN TOTAL NITRATE" |
            VARIABLE == "NITROGEN DISSOLVED NITRATE" |
-           VARIABLE == "NITRITE AND NITRATE UNFILTERED" |
+           #VARIABLE == "NITRITE AND NITRATE UNFILTERED" |
            VARIABLE == "NITRITE AND NITRATE (AS N)" |
-           VARIABLE == "NITRITE & NITRATE UNFILTERED" |
+           #VARIABLE == "NITRITE & NITRATE UNFILTERED" |
            VARIABLE == "NITRATE AND NITRITE (AS N)" |
            VARIABLE == "NITRATE (AS N)" |
-         VARIABLE == "PHOSPHORUS TOTAL") %>%
+           VARIABLE == "PHOSPHATE (AS P)" |
+           VARIABLE == "ORTHOPHOSPHATE DISSOLVED") %>%
   mutate(UNIT = UNIT_UNITE,
          UNIT = ifelse(is.na(UNIT), UNIT_UNITÉ, UNIT),
          UNIT = ifelse(is.na(UNIT), `UNIT_UNITÃ‰`, UNIT))
+
+#do I need to filter for mg/L as P or not?
+check <- CA_data %>%
+  filter(VARIABLE == "ORTHOPHOSPHATE DISSOLVED")
+#Nope! all these are as P :) 
 
 #check parameters and units
 params <- as.data.frame(unique(CA_data$VARIABLE))
 units <- as.data.frame(unique(CA_data$UNIT))
 
 
+
+#renaming variables for continuity
 CA_data1 <- CA_data %>%
   select(DATE_TIME_HEURE, SITE_NO, VALUE_VALEUR, UNIT, VARIABLE) %>%
   rename(DATE_COL = DATE_TIME_HEURE, 
@@ -57,11 +64,13 @@ CA_data1 <- CA_data %>%
                              VARIABLE == "NITROGEN, NITRATE" |
                              VARIABLE == "NITROGEN TOTAL NITRATE" |
                              VARIABLE == "NITROGEN DISSOLVED NITRATE" |
-                             VARIABLE == "NITRITE AND NITRATE UNFILTERED" |
+                             #VARIABLE == "NITRITE AND NITRATE UNFILTERED" |
                              VARIABLE == "NITRITE AND NITRATE (AS N)" |
-                             VARIABLE == "NITRITE & NITRATE UNFILTERED" |
+                             #VARIABLE == "NITRITE & NITRATE UNFILTERED" |
                              VARIABLE == "NITRATE AND NITRITE (AS N)" |
                              VARIABLE == "NITRATE (AS N)", "NO3 as N", VARIABLE)) %>%
+  mutate(VARIABLE = ifelse(VARIABLE == "PHOSPHATE (AS P)" |
+                             VARIABLE == "ORTHOPHOSPHATE DISSOLVED", "PO4 as P", VARIABLE)) %>%
   mutate(DATE_COL = as.Date(DATE_COL)) %>%
   distinct() %>%
   drop_na() %>%
@@ -76,8 +85,7 @@ CA_data2 <- CA_data1 %>%
   pivot_wider(id_cols = c(DATE_COL, SITE_ID), names_from = VARIABLE, values_from = RESULT) %>%
   drop_na() %>%
   mutate(UNITS = "mg/L") %>%
-  rename(DOC = `CARBON DISSOLVED ORGANIC`,
-         TP = `PHOSPHORUS TOTAL`) 
+  rename(DOC = `CARBON DISSOLVED ORGANIC`)
 
 
 #get site data
@@ -99,10 +107,56 @@ combined <- combined%>%
   mutate(ECO_TYPE = "River/Stream") %>%
   ungroup
 
-write.csv(combined, "Data/Natural_Resources_Canada_Data/SIMPLE_NRC.csv")
+write.csv(combined, "Data/Simplified_datasets_per_source/SIMPLE_NRC.csv")
 
 onlysites <- combined %>%
   select(SITE_ID, LAT, LON, ECO_TYPE) %>%
   distinct()
 
-write.csv(onlysites, "Data/Natural_Resources_Canada_Data/UNIQUE_SITES.csv")
+write.csv(onlysites, "Data/Natural_Resources_Canada_Data/Site_Data/UNIQUE_SITES.csv")
+
+
+################################################################################
+#create dataset with all variables but renamed DOC, PO4 as P, and NO3 as N
+
+all_data <- chems_dataset %>%
+  mutate(VARIABLE = ifelse(VARIABLE == "NITROGEN DISSOLVED NO3 & NO2" |
+                             VARIABLE == "NITROGEN, NITRATE" |
+                             VARIABLE == "NITROGEN TOTAL NITRATE" |
+                             VARIABLE == "NITROGEN DISSOLVED NITRATE" |
+                             #VARIABLE == "NITRITE AND NITRATE UNFILTERED" |
+                             VARIABLE == "NITRITE AND NITRATE (AS N)" |
+                             #VARIABLE == "NITRITE & NITRATE UNFILTERED" |
+                             VARIABLE == "NITRATE AND NITRITE (AS N)" |
+                             VARIABLE == "NITRATE (AS N)", "NO3 as N", VARIABLE)) %>%
+  mutate(VARIABLE = ifelse(VARIABLE == "PHOSPHATE (AS P)" |
+                             VARIABLE == "ORTHOPHOSPHATE DISSOLVED", "PO4 as P", VARIABLE)) %>%
+  mutate(VARIABLE = ifelse(VARIABLE == "CARBON DISSOLVED ORGANIC", "DOC", VARIABLE)) %>%
+  mutate(UNIT = UNIT_UNITE,
+         UNIT = ifelse(is.na(UNIT), UNIT_UNITÉ, UNIT),
+         UNIT = ifelse(is.na(UNIT), `UNIT_UNITÃ‰`, UNIT)) %>%
+  select(DATE_TIME_HEURE, SITE_NO, VALUE_VALEUR, UNIT, VARIABLE) %>%
+  rename(DATE_COL = DATE_TIME_HEURE, 
+         SITE_ID = SITE_NO,
+         RESULT = VALUE_VALEUR) %>%
+  filter(RESULT > 0) %>%
+  distinct() %>%
+  drop_na() %>%
+  group_by(DATE_COL, SITE_ID, VARIABLE, UNIT) %>%
+  summarise(RESULT = mean(RESULT)) %>%
+  ungroup() 
+  
+
+#call in sites data for sites that include DOC, NO3, and PO4
+sites_w_cnp <- read.csv("Data/Natural_Resources_Canada_Data/Site_Data/UNIQUE_SITES.csv")
+  
+all_data2 <- all_data %>%
+  left_join(sites_w_cnp) %>%
+  drop_na(LAT) %>%
+  mutate(UNIT = str_remove(all_data2$UNIT, "Â")) %>%
+  select(-X)
+
+
+write.csv(all_data2, "Data/other_vars_datasets/NRC.csv")
+
+
