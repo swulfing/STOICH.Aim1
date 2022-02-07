d34e09bcc448f0226593eb79a9383f00ced0fe65
LinneaRock <lrock@wisc.edu>
2022-02-07 14:25:56 -0700

    Updated EU dataset to include PO4.

--- Code/LRock/Data_formatting/EU_data.R
+++ Code/LRock/Data_formatting/EU_data.R
@@ -3,16 +3,16 @@
 library(tidyverse)
 library(lubridate)
 
-EU <- read.csv("C:/Users/lrock1/OneDrive/Desktop/EU_waterbase/data.csv")
+EU <- read.csv("Data/EU_waterbase/data.csv")
 
 EU.1 <- EU %>%
   filter(parameterWaterBodyCategory == "LW" | #lakes
            parameterWaterBodyCategory == "RW") %>% # rivers
   filter(observedPropertyDeterminandLabel == "Dissolved organic carbon (DOC)" |
            observedPropertyDeterminandLabel == "Nitrate" |
-           observedPropertyDeterminandLabel == "Total phosphorus") #phosphate is available
+           observedPropertyDeterminandLabel == "Phosphate") #phosphate is available
   
-water <- as.data.frame(unique(EU.1$procedureAnalysedMatrix)) #good, just water samples
+water <- as.data.frame(unique(EU.1$procedureAnalysedMatrix)) #good, just water samples, W and W-DIS (dissolved)
 status <- as.data.frame(unique(EU.1$metadata_observationStatus)) #get rid of Us
 
 EU.2 <- EU.1 %>%
@@ -29,7 +29,7 @@ EU.3 <- EU.2 %>%
   distinct()
 
 #site ids right now are more than just location, some of them differ (maybe based on sampling time or some other factor) so I need to get more detailed info from the spatial dataset
-sites <- read.csv("C:/Users/linne/OneDrive/Desktop/EU_waterbase/spatial.csv")
+sites <- read.csv("Data/EU_waterbase/spatial.csv")
 
 sites.a <- sites %>%
   select(monitoringSiteIdentifier, lat, lon) %>%
@@ -41,11 +41,15 @@ EU.i <- left_join(EU.3, sites.a) %>%
 EU.i.2 <- EU.i %>%
   drop_na() #there are lots of NAs on locational info :( ~10K
 
+
 site_unique <- EU.i.2 %>%
   select(lat, lon) %>%
-  distinct() %>%
+  distinct()
+
+site_unique <- site_unique %>%
   mutate(SITE_ID = paste("EU_", seq(1:nrow(site_unique)), sep = "")) #renaming the SITE_ID because of the issue reported on line 31
   
+
 EU.i.3 <- EU.i.2 %>%
   select(-SITE_ID) %>%
   left_join(site_unique) %>%
@@ -62,7 +66,7 @@ EU.4 <- EU.i.3 %>%
 #the finalized dataset
 EU.5 <- EU.4 %>%
   drop_na() %>%
-  rename(TP = `Total phosphorus`,
+  rename(`PO4 as P` = `Phosphate`,
          `NO3 as N` = Nitrate,
          DOC = `Dissolved organic carbon (DOC)`,
          LAT = lat,
@@ -71,4 +75,64 @@ EU.5 <- EU.4 %>%
   mutate(ECO_TYPE = ifelse(ECO_TYPE == "LW", "Lake", "River/Stream"),
          UNITS = "mg/L")
 
+
+
 write.csv(EU.5, "Data/Simplified_datasets_per_source/SIMPLE_EU.csv")
+
+
+#how many sites that contain DOC, NO3, and PO4? 2602
+check.1 <- EU.5 |>
+  select(SITE_ID, LAT, LON) |>
+  distinct()
+
+
+################################################################################
+#create dataset with all variables but renamed DOC, PO4 as P, and NO3 as N
+
+all_data <- EU |>
+  filter(parameterWaterBodyCategory %in% c("LW", "RW")) |> #lakes & rivers
+  filter(metadata_observationStatus == "A") |>
+  select(ï..monitoringSiteIdentifier, observedPropertyDeterminandLabel, resultUom, phenomenonTimeReferenceYear, resultMeanValue, parameterWaterBodyCategory) %>%
+  rename(SITE_ID = ï..monitoringSiteIdentifier,
+  DATE_COL = phenomenonTimeReferenceYear,
+  VARIABLE = observedPropertyDeterminandLabel) |>
+  mutate(resultMeanValue = ifelse(VARIABLE == "Nitrate", resultMeanValue * 0.2259, resultMeanValue)) |>
+  filter(DATE_COL >= 2000) |>
+  distinct() |>
+  mutate(VARIABLE = ifelse(VARIABLE == 'Nitrate', 'NO3 as N', VARIABLE),
+         VARIABLE = ifelse(VARIABLE == 'Phosphate', 'PO4 as P', VARIABLE),
+         VARIABLE = ifelse(VARIABLE == 'Dissolved organic carbon (DOC)', 'DOC', VARIABLE)) |>
+  mutate(resultUom = ifelse(VARIABLE %in% c('NO3 as N', 'PO4 as P', 'DOC'), "mg/L", resultUom)) 
+  
+units <- as.data.frame(unique(all_data$resultUom))
+
+#only keep sites with all the cnp data
+all_data2 <- all_data |>
+  left_join(sites.a) |> #combine with site info
+  select(-SITE_ID) # just keep lat and long because I renamed the sites on lines 31-50
+
+all_data3 <- all_data2|> 
+  rename(LAT = lat,
+         LON = lon) |> #make sure columns have same names
+  left_join(check.1) |>
+  drop_na(SITE_ID) # get rid of any rows that are not at sites with the essential data
+
+#jsut double check we have all the sites
+check.2 <- all_data3 |>
+  select(SITE_ID, LAT, LON) |>
+  distinct() #YAY! all 2602 are here :) 
+
+#finally, rename columns to match other datasets 
+all_data4 <- all_data3 |>
+  rename(ECO_TYPE = parameterWaterBodyCategory,
+         UNIT = resultUom,
+         RESULT = resultMeanValue) |>
+  mutate(ECO_TYPE = ifelse(ECO_TYPE == "LW", "Lake", "River/Stream"))
+
+#reorder columns:
+all_dataFINAL <- all_data4 |>
+  select(DATE_COL, SITE_ID, VARIABLE, UNIT, RESULT, LAT, LON, ECO_TYPE)
+
+write.csv(all_dataFINAL, "Data/other_vars_datasets/EU.csv")
+
+
