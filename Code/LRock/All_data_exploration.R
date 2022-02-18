299ac80f96670359b047d2bd0156da0ca5e3a6f8
Linnea Ann Rock <lrock1@uwyo.edu>
2022-02-18 15:07:29 -0700

    looking at categories of phosphate

--- Code/LRock/All_data_exploration.R
+++ Code/LRock/All_data_exploration.R
@@ -6,11 +6,27 @@ library(lubridate)
 #call in data:
 source("Code/masterData.R")
 
+PO4_categories <- ALL_CNP |>
+  mutate(PO4.as.P_limit = NA) |>
+  mutate(PO4.as.P_limit = ifelse(PO4.as.P < 0.05, "natural", 
+                                 ifelse(between(PO4.as.P, 0.05, 0.1), "limit",
+                                        #ifelse(between(PO4.as.P, 0.1, 0.5), "medium", 
+                                               ifelse(PO4.as.P > 0.1, "high", PO4.as.P_limit))))
+#plotting limit
+
+ggplot(PO4_categories) +
+  geom_point(aes(DOC, NO3.as.N, color = PO4.as.P_limit)) +
+  scale_color_viridis_d(expression(Posphorus - PO[4]~(mg~L^-1))) +
+  theme_bw() +
+  labs(title = "All data",
+       x = "DOC"~(mg~L^-1)) +
+  ylab(expression(Nitrogen - NO[3]~(mg~L^-1)))+
+  scale_y_log10()
 
 #plotting all the data#####
 ggplot(ALL_CNP) +
-  geom_point(aes(DOC, NO3.as.N, color = TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(DOC, NO3.as.N, color = PO4.as.P)) +
+  scale_color_viridis_c("PO4"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "All data",
     x = "DOC"~(mg~L^-1)) +
@@ -19,8 +35,8 @@ ggplot(ALL_CNP) +
      
 
 ggplot(ALL_CNP %>% filter(ECO_TYPE == "Lake")) +
-  geom_point(aes(DOC, NO3.as.N, color = TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(DOC, NO3.as.N, color = PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Lakes - all data",
        x = "DOC"~(mg~L^-1)) +
@@ -28,8 +44,8 @@ ggplot(ALL_CNP %>% filter(ECO_TYPE == "Lake")) +
 
 
 ggplot(ALL_CNP %>% filter(ECO_TYPE != "Lake")) +
-  geom_point(aes(DOC, NO3.as.N, color = TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(DOC, NO3.as.N, color = PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Rivers/Streams - all data",
        x = "DOC"~(mg~L^-1)) +
@@ -43,11 +59,11 @@ by_year <- ALL_CNP %>%
   group_by(SITE_ID, year(DATE_COL), ECO_TYPE) %>%
   summarise(m.DOC = median(DOC),
             m.NO3 = median(NO3.as.N),
-            m.TP = median(TP))
+            m.PO4.as.P = median(PO4.as.P))
   
 ggplot(by_year) +
-  geom_point(aes(m.DOC, m.NO3, color = m.TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(m.DOC, m.NO3, color = m.PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Yearly medians",
        x = "DOC"~(mg~L^-1)) +
@@ -56,8 +72,8 @@ ggplot(by_year) +
 
 
 ggplot(by_year %>% filter(ECO_TYPE == "Lake")) +
-  geom_point(aes(m.DOC, m.NO3, color = m.TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(m.DOC, m.NO3, color = m.PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Lakes - yearly medians",
        x = "DOC"~(mg~L^-1)) +
@@ -65,8 +81,8 @@ ggplot(by_year %>% filter(ECO_TYPE == "Lake")) +
 
 
 ggplot(by_year %>% filter(ECO_TYPE != "Lake")) +
-  geom_point(aes(m.DOC, m.NO3, color = m.TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(m.DOC, m.NO3, color = m.PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Rivers/Streams - yearly medians",
        x = "DOC"~(mg~L^-1)) +
@@ -81,11 +97,11 @@ by_decade<- ALL_CNP %>%
   group_by(SITE_ID, DECADE, ECO_TYPE) %>%
   summarise(m.DOC = median(DOC),
             m.NO3 = median(NO3.as.N),
-            m.TP = median(TP))
+            m.PO4.as.P = median(PO4.as.P))
 
 ggplot(by_decade) +
-  geom_point(aes(m.DOC, m.NO3, color = m.TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(m.DOC, m.NO3, color = m.PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Decadal medians",
        x = "DOC"~(mg~L^-1)) +
@@ -94,8 +110,8 @@ ggplot(by_decade) +
 
 
 ggplot(by_decade %>% filter(ECO_TYPE == "Lake")) +
-  geom_point(aes(m.DOC, m.NO3, color = m.TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(m.DOC, m.NO3, color = m.PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Lakes - decadal medians",
        x = "DOC"~(mg~L^-1)) +
@@ -103,8 +119,8 @@ ggplot(by_decade %>% filter(ECO_TYPE == "Lake")) +
 
 
 ggplot(by_decade %>% filter(ECO_TYPE != "Lake")) +
-  geom_point(aes(m.DOC, m.NO3, color = m.TP * 1000)) +
-  scale_color_viridis_c("TP"~(mu~g~L^-1)) +
+  geom_point(aes(m.DOC, m.NO3, color = m.PO4.as.P * 1000)) +
+  scale_color_viridis_c("PO4.as.P"~(mu~g~L^-1)) +
   theme_bw() +
   labs(title = "Rivers/Streams - yearly medians",
        x = "DOC"~(mg~L^-1)) +
